e07e48f91880d777d33343833a130698decff8f7
Alexsey Kamenskikh <aakamenskikh@mail.ru>
2026-01-15 16:54:53 +0500

    add EnumDemoApple

---
+++ src/main/java/ru/red/thread_test/enum_test/AppleTest.java
@@ -0,0 +1,9 @@
+package ru.red.thread_test.enum_test;
+
+public enum AppleTest {
+    JONATHAN,
+    GOLDEN_DEL,
+    RED_DEL,
+    WINESAP,
+    CORTLAND
+}
---
+++ src/main/java/ru/red/thread_test/enum_test/EnumDemoApple.java
@@ -0,0 +1,45 @@
+package ru.red.thread_test.enum_test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EnumDemoApple {
+    private static final Logger log = LoggerFactory.getLogger(EnumDemoApple.class);
+
+    public static void main(String[] args) {
+        AppleTest appleTest1, appleTest2, appleTest3;
+        //Получить все порядковые нромера с применением ordinal
+        for (AppleTest a:AppleTest.values()) {
+            log.info("{} {}", a, a.ordinal());
+        }
+
+        appleTest1 = AppleTest.RED_DEL;
+        appleTest2 = AppleTest.GOLDEN_DEL;
+        appleTest3 = AppleTest.RED_DEL;
+
+        //Демонстрация использования compareTo() и equals()
+        if (appleTest1.compareTo(appleTest2) < 0) {
+            log.info(appleTest1 + " находится перед " + appleTest2);
+        }
+
+        if (appleTest1.compareTo(appleTest2) > 0) {
+            log.info(appleTest2 + " находится перед " + appleTest1);
+        }
+
+        if (appleTest1.compareTo(appleTest3) == 0) {
+            log.info(appleTest1 + " равно " + appleTest3);
+        }
+
+        if (!appleTest1.equals(appleTest2)) {
+            log.info("Нет равенства!");
+        }
+
+        if (appleTest1.equals(appleTest3)) {
+            log.info(appleTest1 + " равно " + appleTest3);
+        }
+
+        if (appleTest1 == appleTest3) {
+            log.info(appleTest1 + "==" + appleTest3 );
+        }
+    }
+}
